28247e7b3ca39e9b4e00f81b38afb8781c2330fd
ZiadMahmoudSaad <87072047+ZiadMahmoudSaad@users.noreply.github.com>
2023-09-27 14:33:51 +0300

    Simple Calculator Project

---
+++ Simple_Calculator/Debug/Stm32F103C6_Drivers/MCAL/EXTI/Stm32F103C6_EXTI_Driver.d
@@ -0,0 +1,14 @@
+Stm32F103C6_Drivers/MCAL/EXTI/Stm32F103C6_EXTI_Driver.o: \
+ ../Stm32F103C6_Drivers/MCAL/EXTI/Stm32F103C6_EXTI_Driver.c \
+ D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_EXTI_Driver.h \
+ D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/Inc/Stm32F103x6.h \
+ D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_GPIO_Driver.h \
+ D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_GPIO_Driver.h
+
+D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_EXTI_Driver.h:
+
+D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/Inc/Stm32F103x6.h:
+
+D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_GPIO_Driver.h:
+
+D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_GPIO_Driver.h:
---
+++ Simple_Calculator/Debug/Stm32F103C6_Drivers/MCAL/GPIO/Stm32F103C6_GPIO_Driver.d
@@ -0,0 +1,8 @@
+Stm32F103C6_Drivers/MCAL/GPIO/Stm32F103C6_GPIO_Driver.o: \
+ ../Stm32F103C6_Drivers/MCAL/GPIO/Stm32F103C6_GPIO_Driver.c \
+ D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_GPIO_Driver.h \
+ D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/Inc/Stm32F103x6.h
+
+D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/MCAL/Inc/Stm32F103C6_GPIO_Driver.h:
+
+D:/Projects/Simple_Calculator/Stm32F103C6_Drivers/Inc/Stm32F103x6.h:
